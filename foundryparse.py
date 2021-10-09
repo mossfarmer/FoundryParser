90106da7974b6e942df920992c0b778602c90135
SLYNCH1 <scottflynch@gmail.com>
2021-10-09 01:07:04 -0700

    Works but output does not format yet

--- foundryparse.py
+++ foundryparse.py
@@ -1,4 +1,4 @@
-f = open("fvttcopy.txt","r")
+f = open("newfoundrylog.txt","r")
 import  csv
 import itertools
 import sys
@@ -6,6 +6,7 @@ import re
 from collections import defaultdict
 
 out = open('output.csv','w') 
+sys.stdout = open('output.txt','w')
 out = csv.writer(out, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
 a = f.read()
 b = a.split('---------------------------')
@@ -53,7 +54,7 @@ RollData = {
 Abilities = ['Acrobatics',"Animal Handling",'Arcana','Athletics','Deception','History','Insight','Intimidation','Investigation','Medicine','Nature','Perception','Performance','Persuasion','Religion',"Sleight of Hand",'Stealth']
 PCList = ["Zoz","Tilikuss","Mikael","Malus","Alchemical"] 
 BadBoyList = ['Jack','Josh','Will','Zev','Gamemaster']
-XD = ['Average Attack Roll','Initiative','Strength Saving Throw','Dexterity Saving Throw','Constitution Saving Throw','Intelligence Saving Throw','Wisdom Saving Throw','Charisma Saving Throw']
+XD = ['Average Attack Roll','Initiative','Strength Save','Dexterity Save','Constitution Save','Intelligence Save','Wisdom Save','Charisma Save']
 
 BIGLIST = Abilities + XD
 #print(b[0])
@@ -106,8 +107,8 @@ def handleAbilityCheck(rolltype):
     #print(a)
     #print(RollData.get(name).get(skill)[1])
 def handleAttackRoll(roll):
-    if('Alchemical' in roll): #DEBUG
-        print(roll)
+   # if('Alchemical' in roll): #DEBUG
+       # print(roll)
     d = roll.split()           
     name = d[3]
     weapon = d[5]
@@ -122,23 +123,33 @@ def handleAttackRoll(roll):
             else:
                 RollData.get(name).get(weapon)[0]+=1
                 RollData.get(name).get(weapon)[1]+=int(attackroll)
-    except
-    else:
-        RollData.get(name).get('Average Attack Roll')[0]+=1
-        RollData.get(name).get('Average Attack Roll')[1]+=int(attackroll)
+        else:
+            RollData.get(name).get('Average Attack Roll')[0]+=1
+            RollData.get(name).get('Average Attack Roll')[1]+=int(attackroll)
     except IndexError:
+        pass
+   
 
     
     #print(b)
     #print(d[b+1])
     #print(d[3])
+def handleInitiativeRoll(roll):
+    d = roll.split()
+    e = roll.split('=')
+    name = d[3]
+    #print(d[-1])
+    #print(e[1].split()[0])
+    RollData.get(name).get('Initiative')[0]+=1
+    RollData.get(name).get('Initiative')[1]+=int(e[1].split()[0])
+
+    #print(roll.split())
 def determineRollType(roll):
     d = roll.split()
-    if('Alchemical' in roll): #DEBUG
-        print(roll)
+    
     #print(d[6])
     if d[3] in PCList and d[3] not in RollData:
-        RollData.update({d[3]: { 'Initiative' : [0,0],'Average Attack Roll': [0,0],'Strength Saving Throw':[0,0],'Dexterity Saving Throw':[0,0],'Constitution Saving Throw':[0,0],'Intelligence Saving Throw':[0,0],'Wisdom Saving Throw':[0,0],'Charisma Saving Throw':[0,0],'Acrobatics':[0,0],'Animal Handling':[0,0],'Arcana':[0,0],'Athletics':[0,0],'Deception':[0,0],'History':[0,0],'Insight':[0,0],'Intimidation':[0,0],'Investigation':[0,0],'Medicine':[0,0],'Nature':[0,0],'Perception':[0,0],'Performance':[0,0],'Persuasion':[0,0],'Religion':[0,0],'Sleight of Hand':[0,0],'Stealth':[0,0] } })  
+        RollData.update({d[3]: { 'Initiative' : [0,0],'Average Attack Roll': [0,0],'Strength Save':[0,0],'Dexterity Save':[0,0],'Constitution Save':[0,0],'Intelligence Save':[0,0],'Wisdom Save':[0,0],'Charisma Save':[0,0],'Acrobatics':[0,0],'Animal Handling':[0,0],'Arcana':[0,0],'Athletics':[0,0],'Deception':[0,0],'History':[0,0],'Insight':[0,0],'Intimidation':[0,0],'Investigation':[0,0],'Medicine':[0,0],'Nature':[0,0],'Perception':[0,0],'Performance':[0,0],'Persuasion':[0,0],'Religion':[0,0],'Sleight of Hand':[0,0],'Stealth':[0,0] } })  
     if d[3] not in PCList and d[3] not in RollData:
         RollData.update({d[3]: { 'Initiative' : [0,0],'Average Attack Roll':[0,0],'Saving Throws':[0,0],'Skills':[0,0]}})
     if d[5] in Abilities:
@@ -148,24 +159,58 @@ def determineRollType(roll):
     if 'Attack' in roll:
         #print("attack")
         handleAttackRoll(roll)
+    elif 'Initiative!' in roll:
+        handleInitiativeRoll(roll)
+    elif 'Save' in roll:
+        handleSavingThrow(roll)
     #if d[3]+" " + d[4] not in PCList: DEBUG EXAMPLE
         #print(d[3]+" "+d[4])
     #if d[3] in PCList:
-
-
+def handleSavingThrow(roll):
+    d = roll.split()
+    name = d[3]
+    savetype = d[5]+ " " + d[6] 
+    try:
+        ind = [i for i,n in enumerate(d) if n == '1d20'][1] #second occurence of 1 d20 
+        save = d[ind+1]
+        if name in PCList:
+            RollData.get(name).get(savetype)[0]+=1
+            RollData.get(name).get(savetype)[1]+=int(save)
+           
+        else:
+            RollData.get(name).get('Saving Throws')[0]+=1
+            RollData.get(name).get('Saving Throws')[1]+=int(save)
+    except IndexError:
+        pass
+def printoutuput(data):
+    for i in RollData.keys():
+        print(i,sep='')
+        print(RollData.get(i))
+        print('\n')
+def prepareforcsv(data):
+    for i in RollData.keys():
+        for x in RollData.get(i).keys():
+            if int(RollData.get(i).get(x)[0]) != 0:
+                print(i + " " + x + " " +  str(float(RollData.get(i).get(x)[1]/float(RollData.get(i).get(x)[0]))) + "\t\t\t" + str(RollData.get(i).get(x)[1]) +" Out Of " + str(RollData.get(i).get(x)[0])+ " Rolls ")
 if __name__ == '__main__':
-    for i in range(0,55):
+    for i in range(0,len(b)):
+        #if 'Save' in b[i]:
+          #  print(b[i])
+        #print(b[10].split())
         #print(len(b[30].split()))
         #print((b[30].split()))
         #print(b[52].split())
         if len(b[i].split()) < 7 or b[i].split()[3] in BadBoyList or '/roll' in b[i]:
             continue
         
-        print(i)
+        #print(i)
+        #print(b[i])
         determineRollType(b[i])
-        #print(RollData)
+    
+    printoutuput(RollData)
+    prepareforcsv(RollData)
     #print(len(b[1325].split()))
     #print(RollData)
-    
+    sys.stdout.close()    
     
 
